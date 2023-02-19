f53b9cc4ea4ef6e3701738e1f108cfcc64b5a047
Dat HQ Home PC <hoquocdat1310@gmail.com>
2023-02-19 22:33:59 +0700

    First commit and setup project

---
+++ views/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const expressHandlebars = require('express-handlebars');
+const port = process.env.PORT || 5000;
+const app = express()
+
+app.use(express.static( 'public'));
+
+//express-handlebars
+app.engine('hbs', expressHandlebars.engine({
+    layoutsDir: __dirname + '/layouts',
+    partialsDir: __dirname + '/partials',
+    extname: 'hbs',
+    defaultLayout: 'layout'
+}));
+
+// view engine setup
+app.set('view engine', 'hbs');
+//app.set('views', './views/partials')
+
+//routes
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/:page', (req, res) => {
+    res.render(req.params.page);
+})
+
+//start web server
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+})
