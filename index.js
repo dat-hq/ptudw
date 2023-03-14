0bcd7d9b4b59ab6e2b9ed72c0d27ac5108092c24
Dat HQ Home PC <hoquocdat1310@gmail.com>
2023-03-14 21:37:44 +0700

    Display recent product from database

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const expressHandlebars = require('express-handlebars');
-const {createStarList} = require('./controllers/handlebarsHelper');
+const { createStarList } = require('./controllers/handlebarsHelper');
 
 
 app.use(express.static('public'));
