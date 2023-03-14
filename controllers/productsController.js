a930c28dfc69fe85a49d05a65e3d14e6850c9d40
Dat HQ Home PC <hoquocdat1310@gmail.com>
2023-03-14 23:18:59 +0700

    Display product by brand

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -3,6 +3,7 @@ const models = require('../models');
 
 controller.show = async (req, res) => {
     let category = isNaN(req.query.category) ? 0 : parseInt(req.query.category);
+    let brand = isNaN(req.query.brand) ? 0 : parseInt(req.query.brand);
 
     let categories = await models.Category.findAll({
         include: [{
@@ -11,15 +12,27 @@ controller.show = async (req, res) => {
     });
     res.locals.categories = categories;
 
+    let brands = await models.Brand.findAll({
+        include: [{
+            model: models.Product
+        }]
+    });
+    res.locals.brands = brands;
+
+
     let options = {
         attributes: ['id', 'name', 'imagePath', 'stars', 'price', 'oldPrice'],
         where: {}
     };
-    if (category > 0){
+    if (category > 0) {
         options.where.categoryId = category;
+    }
+    if (brand > 0) {
+        options.where.brandId = brand;
     }
     let products = await models.Product.findAll(options);
     res.locals.products = products;
     res.render('product-list');
+
 }
 module.exports = controller;
