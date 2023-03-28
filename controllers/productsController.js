83872d3d67bb1c3da237142e3952d675d44682a7
Dat HQ Home PC <hoquocdat1310@gmail.com>
2023-03-28 22:42:23 +0700

    add function search product

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,7 +1,9 @@
 let controller = {};
 const models = require('../models');
+const sequelize = require('sequelize');
+const Op = sequelize.Op;
 
-controller.getData = async(req,res, next) =>{
+controller.getData = async (req, res, next) => {
     let categories = await models.Category.findAll({
         include: [{
             model: models.Product
@@ -26,7 +28,8 @@ controller.show = async (req, res) => {
     let category = isNaN(req.query.category) ? 0 : parseInt(req.query.category);
     let brand = isNaN(req.query.brand) ? 0 : parseInt(req.query.brand);
     let tag = isNaN(req.query.tag) ? 0 : parseInt(req.query.tag);
-    
+    let keyword = req.query.keyword || '';
+
     let options = {
         attributes: ['id', 'name', 'imagePath', 'stars', 'price', 'oldPrice'],
         where: {}
@@ -42,6 +45,11 @@ controller.show = async (req, res) => {
             model: models.Tag,
             where: { id: tag }
         }];
+    }
+    if (keyword.trim() != '') {
+        options.where.name = {
+            [Op.iLike]: `%${keyword}%`
+        }
     }
 
     let products = await models.Product.findAll(options);
