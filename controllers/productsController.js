6a7700370c676d914aecf0145b1fa1f53753ac1b
Dat HQ Home PC <hoquocdat1310@gmail.com>
2023-03-29 22:19:34 +0700

    Sort Product by Price, Popular and Newest from product list and category

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -29,6 +29,7 @@ controller.show = async (req, res) => {
     let brand = isNaN(req.query.brand) ? 0 : parseInt(req.query.brand);
     let tag = isNaN(req.query.tag) ? 0 : parseInt(req.query.tag);
     let keyword = req.query.keyword || '';
+    let sort = ['price', 'newest', 'popular'].includes(req.query.sort) ? req.query.sort: 'price';
 
     let options = {
         attributes: ['id', 'name', 'imagePath', 'stars', 'price', 'oldPrice'],
@@ -50,6 +51,22 @@ controller.show = async (req, res) => {
         options.where.name = {
             [Op.iLike]: `%${keyword}%`
         }
+    }
+    switch (sort) {
+        case 'newest':
+            options.order = [['createdAt', 'DESC']];
+            break;
+        case 'popular':
+            options.order = [['stars', 'DESC']];
+            break;
+        default:
+            options.order = [['price', 'ASC']];
+    }
+
+    res.locals.sort = sort;
+    res.locals.originalUrl = removeParam('sort', req.originalUrl);
+    if(Object.keys(req.query).length==0){
+        res.locals.originalUrl= res.locals.originalUrl + "?";
     }
 
     let products = await models.Product.findAll(options);
@@ -79,5 +96,23 @@ controller.showDetails = async (req, res) => {
     });
     res.locals.product = product;
     res.render('product-detail');
+}
+
+function removeParam(key, sourceURL) {
+    var rtn = sourceURL.split("?")[0],
+        param,
+        params_arr = [],
+        queryString = (sourceURL.indexOf("?") !== -1) ? sourceURL.split("?")[1] : "";
+    if (queryString !== "") {
+        params_arr = queryString.split("&");
+        for (var i = params_arr.length - 1; i >= 0; i -= 1) {
+            param = params_arr[i].split("=")[0];
+            if (param === key) {
+                params_arr.splice(i, 1);
+            }
+        }
+        if (params_arr.length) rtn = rtn + "?" + params_arr.join("&");
+    }
+    return rtn;
 }
 module.exports = controller;
