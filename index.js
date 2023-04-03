c77d8b821dc6ccd689809ba12ca19f9497612165
Dat HQ Home PC <hoquocdat1310@gmail.com>
2023-04-03 22:34:51 +0700

    Add product into cart

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const port = process.env.PORT || 5000;
 const expressHandlebars = require('express-handlebars');
 const { createStarList } = require('./controllers/handlebarsHelper');
 const { createPagination } = require('express-handlebars-paginate');
+const session = require('express-session');
 
-
+//Cau hinh public static folder
 app.use(express.static('public'));
 
 //express-handlebars
@@ -27,6 +28,29 @@ app.engine('hbs', expressHandlebars.engine({
 app.set('view engine', 'hbs');
 //app.set('views', './views/partials')
 
+//cau hinh doc du lieu post tu body
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+//cau hinh su dung session
+app.use(session({
+    secret: 'S3cret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        maxAge: 20 * 60 * 1000 //20 minutes
+    }
+}));
+
+//middleware khoi tao gio hang
+app.use((req, res, next)=>{
+    let Cart = require('./controllers/cart');
+    req.session.cart = new Cart (req.session.cart ? req.session.cart: {});
+    res.locals.quantity = req.session.cart.quantity;
+
+    next();
+});
 // routes 
 app.use('/', require('./routes/indexRouter'));
 app.use('/products', require('./routes/productsRouter'));
