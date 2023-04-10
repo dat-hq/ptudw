774a36337f833d21671054c3b615b7cacb6c9213
Dat HQ Home PC <hoquocdat1310@gmail.com>
2023-04-10 23:43:11 +0700

    Add function place orders

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -5,4 +5,5 @@ const router = express.Router();
 const controller = require('../controllers/usersController');
 
 router.get('/checkout', controller.checkout);
+router.post('/placeorders', controller.placeorders);
 module.exports = router;
