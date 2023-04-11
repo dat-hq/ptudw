2992cbe540341d5d8c4d5070aecf24092b41d121
Dat HQ Home PC <hoquocdat1310@gmail.com>
2023-04-11 19:15:42 +0700

    Fix code in user router

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -3,25 +3,28 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/usersController');
-const { body, resultValidaton } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 
 router.get('/checkout', controller.checkout);
-router.post('/placeorders',
+router.post('/placeorders', [
     body('firstName').notEmpty().withMessage('First name is required.'),
     body('lastName').notEmpty().withMessage('Last name is required.'),
-    body('email').notEmpty().withMessage('Email is required.').isEmail().withMessage('Invalid Email Address, Please Try Again'),
+    body('email').notEmpty().withMessage('Email is required.').
+        isEmail().withMessage('Invalid Email Address, Please Try Again'),
     body('mobile').notEmpty().withMessage('Mobile No is required.'),
     body('address').notEmpty().withMessage('Address is required.'),
-    (res, req, next) => {
-        let errors = resultValidaton(req);
-        if (req.body.addressId ==0  && !errors.isEmpty()) {
+    (req, res, next) => {
+        let errors = validationResult(req);
+        if (req.body.addressId == "0" && !errors.isEmpty()) {
             let errorArray = errors.array();
+            let message = '';
             for (let i = 0; i < errorArray.length; i++) {
-                message += errorArray[i].msg + "<br/>"
+                message += errorArray[i].msg + "<br/>";
             }
             return res.render('error', { message });
         }
         next();
     },
-    controller.placeorders);
+    controller.placeorders
+]);
 module.exports = router;
